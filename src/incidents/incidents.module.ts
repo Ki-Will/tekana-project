bce812e9412cef65333f45c8934c3f0262db46ae
Ki-Will <irakozep03@gmail.com>
2026-02-26 20:32:22 +0200

    Update src/incidents/incidents.module.ts

--- src/incidents/incidents.module.ts
+++ src/incidents/incidents.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { MessagingModule } from '../messaging/messaging.module';
 import { AuditModule } from '../audit/audit.module';
 import { EmailModule } from '../email/email.module';
+import { MapsModule } from '../maps/maps.module';
 
 @Module({
-  imports: [PrismaModule, MessagingModule, AuditModule, EmailModule],
+  imports: [PrismaModule, MessagingModule, AuditModule, EmailModule, MapsModule],
   controllers: [IncidentsController],
   providers: [IncidentsService],
 })
